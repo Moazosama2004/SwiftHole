c68a7753943b089b403a9692dd52528c563c00d4
Moaz Osama <moazosama204@gmail.com>
2026-04-17 13:14:19 -0700

    finish constants & random & range lesson

--- SwiftHole.playground/Contents.swift
+++ SwiftHole.playground/Contents.swift
@@ -1,4 +1,22 @@
-import UIKit
+/*
+    Problem Constants
+*/
 
-var greeting = "Hello, playground"
-print(greeting)
+let secondsInAnHour = 3600
+
+//value of secondsInAnHour Don't change below.
+print(secondsInAnHour)
+
+/*
+    Problem Random
+*/
+let alphabet : [String] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+//The number of letters in alphabet equals 26
+
+let password : String = String(alphabet.randomElement()!) + String(alphabet.randomElement()!) + String(alphabet.randomElement()!) + String(alphabet.randomElement()!) + String(alphabet.randomElement()!) + String(alphabet.randomElement()!)
+
+print(password)
+
+
+		
