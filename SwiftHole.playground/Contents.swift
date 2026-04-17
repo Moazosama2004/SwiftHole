700c34b3450ce2c50c21e13ddb7e15779dbea69b
Moaz Osama <moazosama204@gmail.com>
2026-04-17 07:36:58 -0700

    finish lesson

--- SwiftHole.playground/Contents.swift
+++ SwiftHole.playground/Contents.swift
@@ -1,4 +1,19 @@
-import UIKit
+/*
+ Naming Convention (we prefer using camelCase)
+ like : numberOfStudents - ageOfPerson
+*/
 
-var greeting = "Hello, playground"
-print(greeting)
+/*
+    Comments :
+    // for single line comment
+    /*
+        for multiple line of something we want to say
+    */
+*/
+
+// Print Statment + String Interpolation
+print("Something we want to say")
+print("Hello World, Swift")
+print("Focus here on 5 + 10  = 15")
+print("it's hardcoded")
+print("but now it's not \(5) + \(10) = \(5+10)")
