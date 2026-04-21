b38d45a26190fc07ca16ea070688b72c8b30f44e
Moaz Osama <moazosama204@gmail.com>
2026-04-21 07:49:09 -0700

    finish switch lesson

--- SwiftHole.playground/Contents.swift
+++ SwiftHole.playground/Contents.swift
@@ -1,4 +1,23 @@
-import UIKit
+// switch statments
 
-var greeting = "Hello, playground"
-print(greeting)
+
+func loveCalculator() -> String{
+    let loveScore = Int.random(in: 1...100)
+    
+    
+    switch loveScore {
+    case 81...100:
+        return "You love each other like Kanye loves Kanye"
+    case 41...80:
+        return "You go together like Coke and Mentos"
+    case ...40:
+        return "You'll be forever alone"
+    default :
+        return "error given"
+    }
+}
+
+print(loveCalculator())
+print(loveCalculator())
+print(loveCalculator())
+
