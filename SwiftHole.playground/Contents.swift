ccf733e41316c4d83e11b8026dde449a324a4aa1
Moaz Osama <moazosama204@gmail.com>
2026-04-21 07:51:07 -0700

    finish dict lesson

--- SwiftHole.playground/Contents.swift
+++ SwiftHole.playground/Contents.swift
@@ -1,23 +1,23 @@
-// switch statments
+func exercise() {
 
+    //Don't change this
+    var stockTickers: [String: String] = [
+        "APPL" : "Apple Inc",
+        "HOG": "Harley-Davidson Inc",
+        "BOOM": "Dynamic Materials",
+        "HEINY": "Heineken",
+        "BEN": "Franklin Resources Inc"
+    ]
+    
+    //Write your code here.
+    stockTickers["WORK"] = "Slack Technologies Inc"
+    stockTickers["BOOM"] = "DMC Global Inc"
 
-func loveCalculator() -> String{
-    let loveScore = Int.random(in: 1...100)
-    
-    
-    switch loveScore {
-    case 81...100:
-        return "You love each other like Kanye loves Kanye"
-    case 41...80:
-        return "You go together like Coke and Mentos"
-    case ...40:
-        return "You'll be forever alone"
-    default :
-        return "error given"
-    }
+  
+     //Don't modify this
+    print(stockTickers["WORK"]!)
+    print(stockTickers["BOOM"]!)
 }
+ 
 
-print(loveCalculator())
-print(loveCalculator())
-print(loveCalculator())
-
+exercise()
