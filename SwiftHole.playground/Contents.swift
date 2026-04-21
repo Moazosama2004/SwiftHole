9c3c8a112c6caecc03b7d665e5763c84d33f9826
Moaz Osama <moazosama204@gmail.com>
2026-04-21 07:30:34 -0700

    finish if else statment

--- SwiftHole.playground/Contents.swift
+++ SwiftHole.playground/Contents.swift
@@ -1,4 +1,35 @@
-import UIKit
+// if - else statments
 
-var greeting = "Hello, playground"
-print(greeting)
+// Challenge 1
+
+func loveCalculatorChallengeOne() -> String{
+    let loveScore = Int.random(in: 1...100)
+    
+    if loveScore == 100 {
+        return "You love each other like Kanye loves Kanye"
+    } else {
+        return "You'll be forever alone"
+    }
+}
+
+print(loveCalculatorChallengeOne())
+print(loveCalculatorChallengeOne())
+print(loveCalculatorChallengeOne())
+
+// Challenge 2
+
+func loveCalculatorChallengeTwo() -> String{
+    let loveScore = Int.random(in: 1...100)
+    
+    if loveScore > 80 {
+        return "You love each other like Kanye loves Kanye"
+    } else if loveScore > 40  {
+        return "You go together like Coke and Mentos"
+    } else {
+        return "You'll be forever alone"
+    }
+}
+
+print(loveCalculatorChallengeTwo())
+print(loveCalculatorChallengeTwo())
+print(loveCalculatorChallengeTwo())
