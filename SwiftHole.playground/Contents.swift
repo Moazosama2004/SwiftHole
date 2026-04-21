14835201669a59792ca0664e16dc3a78160d30a2
Moaz Osama <moazosama204@gmail.com>
2026-04-21 08:21:47 -0700

    finish optional lesson

--- SwiftHole.playground/Contents.swift
+++ SwiftHole.playground/Contents.swift
@@ -1,4 +1,25 @@
-import UIKit
+//Don't change this
+var studentsAndscores = ["Amy": 88, "James": 55, "Helen": 99]
 
-var greeting = "Hello, playground"
-print(greeting)
+func highestScore(scores: [String: Int]?) {
+  //Write your code here.
+    if scores == nil {
+        print("studentsAndscores is Empty")
+        return
+    }
+    
+    if scores!.isEmpty {
+        print("studentsAndscores is Empty")
+        return
+    }
+    let result = scores!.values.max()!
+    print(result)
+  	
+}
+
+
+highestScore(scores: nil)
+highestScore(scores: [:])
+highestScore(scores: ["Amy": 88])
+highestScore(scores: studentsAndscores)
+
