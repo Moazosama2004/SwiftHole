22af0267ab014c096a56c50cded65bbaf2105e33
Moaz Osama <moazosama204@gmail.com>
2026-04-22 08:53:03 -0700

    finish struct lesson

--- SwiftHole.playground/Contents.swift
+++ SwiftHole.playground/Contents.swift
@@ -1,4 +1,38 @@
-import UIKit
+func exercise() {
 
-var greeting = "Hello, playground"
-print(greeting)
+    // Define the User struct here
+    struct User {
+        var name: String
+        let email : String?
+        var followers: Int
+        var isActive: Bool
+        
+        
+        func logStatus() {
+            if isActive {
+                print("\(self.name) is working hard")
+            } else {
+                print("\(self.name) has left earth")
+            }
+        }
+    }
+
+    // Initialise a User struct here
+    var rich = User(name: "Richard", email: nil ,followers: 0 , isActive: false)
+    rich.logStatus()
+
+
+
+    // Diagnostic code - do not change this code
+    print("\nDiagnostic code (i.e., Challenge Hint):")
+    var musk = User(name: "Elon", email: "elon@tesla.com", followers: 2001, isActive: true)
+    musk.logStatus()
+    print("Contacting \(musk.name) on \(musk.email!) ...")
+    print("\(musk.name) has \(musk.followers) followers")
+    // sometime later
+    musk.isActive = false
+    musk.logStatus()
+    
+}
+
+exercise()
